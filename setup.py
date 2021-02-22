e8f444eb6a2dc221f2003f5974c24ffabb2a8e33
Douwe Maan <douwe@gitlab.com>
2021-02-22 16:36:59 +0000

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="target-snowflake",
-    version="0.1.1",
+    version="0.1.2",
     author="Meltano",
     author_email="meltano@gitlab.com",
     description="Singer.io target for importing data to Snowflake",
